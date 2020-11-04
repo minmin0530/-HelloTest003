4b266d72f608c3c3801244cdc5907763efb28e6e
minmin0530 <izumiyoshiki@icloud.com>
2020-11-04 13:42:36 +0900

    corection app

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import { Main } from './Main';
 
 const Tab = createBottomTabNavigator();
@@ -26,38 +25,105 @@ function MessageScreen() {
 }
 
 
-function HomeScreen() {
-   navigationOptions = ({navigation}) => ({
-    title:'Stack1',
-    headerLeft:(
-        <Icon name="bars" size={24} onPress={()=>{navigation.openDrawer()}} style={{paddingLeft:20}}/>
-    ),
-  });
-
+function HomeScreen({ navigation }) {
   return (
+    <>
+      <Button
+        onPress = { () => navigation.navigate('Settings') }
+        title="FButton"
+      />
     <Tab.Navigator>
       <Tab.Screen name="Main" component={Main} />
       <Tab.Screen name="Feed" component={FeedScreen} />
       <Tab.Screen name="Message" component={MessageScreen} />
     </Tab.Navigator>
+    </>
   );
 }
 
-function SettingsScreen() {
+function SettingsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+    <>
+      <Button
+        onPress = { () => navigation.navigate('Home') }
+        title="HButton"
+      />
+      <Tab.Navigator>
+        <Tab.Screen name="Feed" component={FeedScreen} />
+        <Tab.Screen name="Message" component={MessageScreen} />
+      </Tab.Navigator>
+    </>
+  );
+}
+
+var ss = HomeScreen;
+
+function LogoFunc({ navigation }) {
+  const [screen, setScreen] = React.useState(App.state);
+  return (
+    <View>
+      <Button
+        onPress = { () => {
+          console.log(ss)
+          ss = FeedScreen
+          navigation.navigate('Feed')
+
+        } }    
+        title="FFButton"
+      />
     </View>
   );
 }
+class LogoTitle extends React.Component {
+  render() {
+    return (
+      <Button
+        onPress={ () => {
+          console.log(Stack.Screen.props);
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+                            } }
+        title="LButton"
+      />
+    );
+  }
 }
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={ss}
+            options={{
+              headerLeft: props => <LogoFunc />,
+              headerTitle: props => <Text>Title</Text>,
+              headerRight: props => <Button
+                onPress={ ({navigation}) => { navigation.navigate('Feed') } }
+                title='RButton'
+              />,
+            }}
+          />
+          
+          <Stack.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              headerLeft: props => <LogoFunc />,
+              headerTitle: props => <Text>Title</Text>,
+              headerRight: props => <Button
+                onPress={ ({navigation}) => { navigation.navigate('Feed') } }
+                title='RButton'
+              />,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+
+
+
